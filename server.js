83815536fe244be0b24eee2dc276b30f84d3c6c1
alansantos <alanpontoinfo@gmail.com>
2021-05-23 09:47:24 -0300

    add comment app.rout for  connection with database mongodb to server.js

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: true,
@@ -56,16 +62,12 @@ passport.deserializeUser((id,  done)=>{
 
 
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+/*
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug/index', {title: 'Hello', message: 'Please login'});
 
 
-});
+});*/
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
