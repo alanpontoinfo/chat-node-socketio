a305a07ff616b8a8a7117c4bc3e0bbb7f3dcef01
alansantos <alanpontoinfo@gmail.com>
2021-05-23 18:54:24 -0300

    add req user to server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ console.log("Databse conected");
 
 
         app.route('/profile').get(ensureAuthenticated, (req, res) => {
-          res.render(process.cwd() + '/views/pug/profile');
+          res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
         });
       
       
