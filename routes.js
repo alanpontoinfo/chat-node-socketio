7603faa203bbea593ff7f2efbe9c3bc2ff023e75
alansantos <alanpontoinfo@gmail.com>
2021-05-24 11:43:15 -0300

    add require dotenv to auth with  passaport

--- routes.js
+++ routes.js
@@ -45,8 +45,8 @@ module.exports = function (app, myDataBase) {
   /*app.route('/login')
   .post(passport.authenticate('local', { failureRedirect: '/' }), (req,res) => {
     res.redirect('/profile');
-  })
-  app.route('/auth/github').get(passport.authenticate('github'));*/
+  })*/
+  app.route('/auth/github').get(passport.authenticate('github'));
   app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
     res.redirect('/profile');
   });
